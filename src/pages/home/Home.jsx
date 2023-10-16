061b7c1b29b275d8c381d9f3b1e51240f09f516c
Nikhil <manginikhil2003@gmail.com>
2023-10-17 01:19:46 +0530

    finalmain-1.1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./style.scss"
-import { TopRated } from './toprated/TopRated'
+import { TopRated } from './topRated/TopRated'
 import { Popular } from './popular/Popular'
 import {Trending}  from './trending/Trending'
 import HeroBanner  from './heroBanner/HeroBanner'
